a858a36deb4f7178b3a088560748f88de0b0a76c
agustinnadalich <agustin.nadalich@gmail.com>
2023-04-18 01:16:24 +0200

    added infractions charts

--- lib/team_stats_page.dart
+++ lib/team_stats_page.dart
@@ -6,17 +6,13 @@ import 'package:match_recorder/widgets/teams/team_save_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:match_recorder/widgets/stats/player_stats.dart';
-// import 'package:match_recorder/widgets/stats/chartTitle.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 // enum TeamType { team1, team2 }
 
 class TeamStatsPage extends StatelessWidget {
   // final TeamType teamType2;
   // const TeamStatsPage({Key? key, required this.teamType2}) : super(key: key);
-
-  final chartTitle = charts.ChartTitle('My Chart Title',
-      behaviorPosition: charts.BehaviorPosition.top,
-      titleOutsideJustification: charts.OutsideJustification.middleDrawArea);
 
   @override
   Widget build(BuildContext context) {
@@ -31,27 +27,94 @@ class TeamStatsPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              height: 300,
+              height: 200,
               child: charts.BarChart(
                 seriesList,
-                animate: true,
+                animate: false,
                 vertical: true,
-                behaviors: [chartTitle],
                 barGroupingType: charts.BarGroupingType.stacked,
+                barRendererDecorator: charts.BarLabelDecorator(
+                    labelAnchor: charts.BarLabelAnchor.middle,
+                    labelPosition: charts.BarLabelPosition.inside),
                 domainAxis: charts.OrdinalAxisSpec(
-                    renderSpec:
-                        charts.SmallTickRendererSpec(labelRotation: 60)),
-                primaryMeasureAxis: charts.NumericAxisSpec(
-                  renderSpec: charts.GridlineRendererSpec(
-                    labelStyle: charts.TextStyleSpec(
-                        fontSize: 12,
-                        color: charts.MaterialPalette.gray.shadeDefault),
-                    lineStyle: charts.LineStyleSpec(
-                        color: charts.MaterialPalette.gray.shade100),
+                  renderSpec: charts.SmallTickRendererSpec(
+                    labelRotation: 60,
                   ),
                 ),
+                primaryMeasureAxis: charts.NumericAxisSpec(
+                    renderSpec: charts.GridlineRendererSpec(
+                        // labelStyle: charts.TextStyleSpec(
+                        //     fontSize: 12,
+                        //     color: charts.MaterialPalette.gray.shadeDefault),
+                        // lineStyle: charts.LineStyleSpec(
+                        //     color: charts.MaterialPalette.gray.shade100),
+                        )),
+                // primaryMeasureAxis: charts.NumericAxisSpec(),
+                behaviors: [
+                  new charts.ChartTitle('Tackles by player',
+                      // subTitle: 'Top sub-title text',
+                      behaviorPosition: charts.BehaviorPosition.top,
+                      titleOutsideJustification:
+                          charts.OutsideJustification.start,
+                      innerPadding: 18),
+                ],
               ),
             ),
+            Container(
+              height: 200,
+              child: charts.BarChart(
+                seriesList2,
+                animate: false,
+                vertical: true,
+                barGroupingType: charts.BarGroupingType.stacked,
+                barRendererDecorator: charts.BarLabelDecorator(
+                    labelAnchor: charts.BarLabelAnchor.middle,
+                    labelPosition: charts.BarLabelPosition.inside),
+                domainAxis: charts.OrdinalAxisSpec(
+                  renderSpec: charts.SmallTickRendererSpec(labelRotation: 60),
+                ),
+                // primaryMeasureAxis: charts.NumericAxisSpec(),
+                behaviors: [
+                  new charts.ChartTitle('Infractions by player',
+                      // subTitle: 'Top sub-title text',
+                      behaviorPosition: charts.BehaviorPosition.top,
+                      titleOutsideJustification:
+                          charts.OutsideJustification.start,
+                      innerPadding: 18),
+                ],
+              ),
+            ),
+            Container(
+                height: 200,
+                child: PieChart(
+                  dataMap: teamInfractionsMap,
+                  animationDuration: Duration(milliseconds: 800),
+                  chartLegendSpacing: 32,
+                  chartRadius: MediaQuery.of(context).size.width / 3.2,
+                  // colorList: colorList,
+                  initialAngleInDegree: 0,
+                  chartType: ChartType.disc,
+                  ringStrokeWidth: 32,
+                  centerText: "Team Infractions",
+                  legendOptions: LegendOptions(
+                    showLegendsInRow: false,
+                    legendPosition: LegendPosition.right,
+                    showLegends: true,
+                    // legendShape: _BoxShape.circle,
+                    legendTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  chartValuesOptions: ChartValuesOptions(
+                    showChartValueBackground: true,
+                    showChartValues: true,
+                    showChartValuesInPercentage: false,
+                    showChartValuesOutside: false,
+                    decimalPlaces: 1,
+                  ),
+                  // gradientList: ---To add gradient colors---
+                  // emptyColorGradient: ---Empty Color gradient---
+                )),
           ],
         ),
       ),
--- lib/widgets/stats/player_stats.dart
+++ lib/widgets/stats/player_stats.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:match_recorder/enums/descriptors.dart';
 
-class PlayerData {
+class PlayerDataTackles {
   final String name;
   final int appearances;
   final int positiveEvents;
   final int negativeEvents;
   final int neutralEvents;
 
-  PlayerData({
+  PlayerDataTackles({
     required this.name,
     required this.appearances,
     required this.positiveEvents,
@@ -18,20 +19,52 @@ class PlayerData {
   });
 }
 
-List<PlayerData> playerDataList = [
-  PlayerData(
+class PlayerDataInfractions {
+  final String name;
+  final int infractions;
+  final int offside;
+  final int ruckAttack;
+  final int ruckDefense;
+
+  PlayerDataInfractions({
+    required this.name,
+    required this.infractions,
+    required this.offside,
+    required this.ruckAttack,
+    required this.ruckDefense,
+  });
+}
+
+class TeamDataInfractions {
+  final String name;
+  final int infractions;
+  final int offside;
+  final int ruckAttack;
+  final int ruckDefense;
+
+  TeamDataInfractions({
+    required this.name,
+    required this.infractions,
+    required this.offside,
+    required this.ruckAttack,
+    required this.ruckDefense,
+  });
+}
+
+List<PlayerDataTackles> playerDataList = [
+  PlayerDataTackles(
       name: 'Player 1',
       appearances: 5,
       positiveEvents: 4,
       negativeEvents: 1,
       neutralEvents: 0),
-  PlayerData(
+  PlayerDataTackles(
       name: 'Player 2',
       appearances: 5,
       positiveEvents: 0,
       negativeEvents: 4,
       neutralEvents: 1),
-  PlayerData(
+  PlayerDataTackles(
       name: 'Player 3',
       appearances: 3,
       positiveEvents: 1,
@@ -40,26 +73,115 @@ List<PlayerData> playerDataList = [
   // Add more players here
 ];
 
-List<charts.Series<PlayerData, String>> seriesList = [
-  charts.Series<PlayerData, String>(
-    id: 'Positive',
-    data: playerDataList,
-    domainFn: (PlayerData data, _) => data.name,
-    measureFn: (PlayerData data, _) => data.positiveEvents,
-    colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.green),
-  ),
-  charts.Series<PlayerData, String>(
-    id: 'Negative',
-    data: playerDataList,
-    domainFn: (PlayerData data, _) => data.name,
-    measureFn: (PlayerData data, _) => data.negativeEvents,
-    colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.red),
-  ),
-  charts.Series<PlayerData, String>(
-    id: 'Neutral',
-    data: playerDataList,
-    domainFn: (PlayerData data, _) => data.name,
-    measureFn: (PlayerData data, _) => data.neutralEvents,
-    colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.yellow),
-  )
+List<PlayerDataInfractions> playerDataList2 = [
+  PlayerDataInfractions(
+      name: 'Player 1',
+      infractions: 8,
+      offside: 4,
+      ruckAttack: 1,
+      ruckDefense: 3),
+  PlayerDataInfractions(
+      name: 'Player 2',
+      infractions: 5,
+      offside: 2,
+      ruckAttack: 0,
+      ruckDefense: 3),
+  PlayerDataInfractions(
+      name: 'Player 3',
+      infractions: 3,
+      offside: 1,
+      ruckAttack: 1,
+      ruckDefense: 1),
+  // Add more players here
+];
+
+Map<String, double> teamInfractionsMap = {
+  "offside": 8,
+  "ruckAttack": 3,
+  "ruckDefense": 2,
+};
+
+List<charts.Series<PlayerDataTackles, String>> seriesList = [
+  charts.Series<PlayerDataTackles, String>(
+      id: 'Positive',
+      data: playerDataList,
+      domainFn: (PlayerDataTackles data, _) => data.name,
+      measureFn: (PlayerDataTackles data, _) => data.positiveEvents,
+      colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.green),
+      // labelAccessorFn: _getLabel,
+      labelAccessorFn: (PlayerDataTackles data, _) {
+        if (data.positiveEvents != 0) {
+          return '${data.positiveEvents}';
+        } else {
+          return '';
+        }
+      }),
+  charts.Series<PlayerDataTackles, String>(
+      id: 'Negative',
+      data: playerDataList,
+      domainFn: (PlayerDataTackles data, _) => data.name,
+      measureFn: (PlayerDataTackles data, _) => data.negativeEvents,
+      colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.red),
+      labelAccessorFn: (PlayerDataTackles data, _) {
+        if (data.negativeEvents != 0) {
+          return '${data.negativeEvents}';
+        } else {
+          return '';
+        }
+      }),
+  charts.Series<PlayerDataTackles, String>(
+      id: 'Neutral',
+      data: playerDataList,
+      domainFn: (PlayerDataTackles data, _) => data.name,
+      measureFn: (PlayerDataTackles data, _) => data.neutralEvents,
+      colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.yellow),
+      labelAccessorFn: (PlayerDataTackles data, _) {
+        if (data.neutralEvents != 0) {
+          return '${data.neutralEvents}';
+        } else {
+          return '';
+        }
+      }),
+];
+
+List<charts.Series<PlayerDataInfractions, String>> seriesList2 = [
+  charts.Series<PlayerDataInfractions, String>(
+      id: 'Offside',
+      data: playerDataList2,
+      domainFn: (PlayerDataInfractions data, _) => data.name,
+      measureFn: (PlayerDataInfractions data, _) => data.offside,
+      colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.green),
+      labelAccessorFn: (PlayerDataInfractions data, _) {
+        if (data.offside != 0) {
+          return '${data.offside}';
+        } else {
+          return '';
+        }
+      }),
+  charts.Series<PlayerDataInfractions, String>(
+      id: 'Ruck Defense',
+      data: playerDataList2,
+      domainFn: (PlayerDataInfractions data, _) => data.name,
+      measureFn: (PlayerDataInfractions data, _) => data.ruckAttack,
+      colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.red),
+      labelAccessorFn: (PlayerDataInfractions data, _) {
+        if (data.ruckAttack != 0) {
+          return '${data.ruckAttack}';
+        } else {
+          return '';
+        }
+      }),
+  charts.Series<PlayerDataInfractions, String>(
+      id: 'Ruck Attack',
+      data: playerDataList2,
+      domainFn: (PlayerDataInfractions data, _) => data.name,
+      measureFn: (PlayerDataInfractions data, _) => data.ruckDefense,
+      colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.yellow),
+      labelAccessorFn: (PlayerDataInfractions data, _) {
+        if (data.ruckDefense != 0) {
+          return '${data.ruckDefense}';
+        } else {
+          return '';
+        }
+      })
 ];
